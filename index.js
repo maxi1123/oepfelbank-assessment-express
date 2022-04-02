62cd399ba66348ae139d7ce1cefaab3bc7556009
Maximilian Jonescu <maximilian.jonescu@uzh.ch>
2022-04-02 15:47:28 +0200

    feature: add GET accounts/:id

--- index.js
+++ index.js
@@ -5,6 +5,60 @@ import BASE_URL from "./constants.js";
 const app = Express();
 const port = 3000;
 
+app.get(`${BASE_URL}/accounts/:id`, async (req, res) => {
+    const id = req.params.id;
+    try {
+        const bearerToken = req.headers.authorization.split(" ")[1];
+        const headerConfig = {
+            headers: {
+                Authorization: `Bearer ${bearerToken}`,
+            },
+        };
+
+        const accountResponse = await axios.get(
+            "https://ob.sandbox.natwest.com/open-banking/v3.1/aisp/accounts/",
+            headerConfig
+        );
+
+        let selectedAccount = {};
+
+        accountResponse.data.Data.Account.map((e) => {
+            if (e.AccountId === id) {
+                selectedAccount = {
+                    accountId: e.AccountId,
+                    accountSubType:
+                        e.AccountSubType === "CurrentAccount"
+                            ? "Current Account"
+                            : e.AccountSubType,
+                    currency: e.Currency,
+                };
+            }
+        });
+
+        const balanceResponse = await axios.get(
+            `https://ob.sandbox.natwest.com/open-banking/v3.1/aisp/accounts/${id}/balances`,
+            headerConfig
+        );
+
+        const selectedAccountBalance = (
+            Math.ceil(
+                parseFloat(balanceResponse.data.Data.Balance[0].Amount.Amount) *
+                    20 -
+                    0.5
+            ) / 20
+        ).toFixed(2);
+
+        selectedAccount.balance = selectedAccountBalance;
+        res.json(selectedAccount);
+    } catch (error) {
+        if (error.response.status === 400) {
+            res.status(400).send(
+                "We could not find any account with given ID."
+            );
+        }
+    }
+});
+
 app.get(`${BASE_URL}/accounts`, async (req, res) => {
     const bearerToken = req.headers.authorization.split(" ")[1];
 
@@ -24,7 +78,10 @@ app.get(`${BASE_URL}/accounts`, async (req, res) => {
     accountResponse.data.Data.Account.map((e) => {
         accountResponseArray.push({
             accountId: e.AccountId,
-            accountSubType: e.AccountSubType,
+            accountSubType:
+                e.AccountSubType === "CurrentAccount"
+                    ? "Current Account"
+                    : e.AccountSubType,
             currency: e.Currency,
         });
     });
@@ -74,6 +131,7 @@ app.get(`${BASE_URL}/transactions/:id`, async (req, res) => {
         );
 
         const transactionsResponseArray = [];
+        let total = 0;
 
         const getDate = (dateString) => {
             const date = new Date(dateString);
@@ -86,9 +144,13 @@ app.get(`${BASE_URL}/transactions/:id`, async (req, res) => {
             return transformed;
         };
 
-        console.log(transactionsResponse.data);
-
         transactionsResponse.data.Data.Transaction.map((e) => {
+            total += parseFloat(
+                (
+                    Math.ceil(parseFloat(e.Amount.Amount) * 20 - 0.5) / 20
+                ).toFixed(2)
+            );
+
             transactionsResponseArray.push({
                 transactionId: e.TransactionId,
                 transactionInfo: e.TransactionInformation,
@@ -104,6 +166,7 @@ app.get(`${BASE_URL}/transactions/:id`, async (req, res) => {
         res.json({
             accountId: id,
             transactions: transactionsResponseArray,
+            total: total.toFixed(2),
         });
     } catch (error) {
         if (error.response.status === 400) {
